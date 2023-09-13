f11c2bb988335328a5ec94df74174334ea23ffe8
Gabriel Nakaoka <gabrielnakaoka_99@hotmail.com>
2023-09-13 00:45:57 -0300

    init commit

---
+++ config/env/development/database.ts
@@ -0,0 +1,13 @@
+export default ({ env }) => ({
+	connection: {
+		client: 'postgres',
+		connection: {
+		host: env('DATABASE_HOST', 'localhost'),
+			port: env.int('DATABASE_PORT', 5432),
+			database: env('DATABASE_NAME', 'sharp-admin'),
+			user: env('DATABASE_USERNAME', 'sharp'),
+			password: env('DATABASE_PASSWORD', 'sharp'),
+			ssl: env.bool('DATABASE_SSL', false)
+		}
+	}
+});
