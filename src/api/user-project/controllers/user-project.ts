763b72db72af7fb7d790b461f6490273be9c4b13
NakaokaGabriel <gabrielnakaoka_99@hotmail.com>
2024-02-14 21:48:31 -0300

    feat: created relation with project and user

---
+++ src/api/user-project/controllers/user-project.ts
@@ -0,0 +1,7 @@
+/**
+ * user-project controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::user-project.user-project');
---
+++ src/api/user-project/routes/user-project.ts
@@ -0,0 +1,7 @@
+/**
+ * user-project router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::user-project.user-project');
