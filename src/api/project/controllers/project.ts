af641d96dc98251c4f25867f1948a1051e93ff95
NakaokaGabriel <gabrielnakaoka_99@hotmail.com>
2024-02-15 22:50:19 -0300

    chore: :sparkles: new entities and create new fields to projects and process

--- src/api/project/controllers/project.ts
+++ src/api/project/controllers/project.ts
@@ -4,4 +4,16 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::project.project');
+export default factories.createCoreController('api::project.project', () => ({
+  async findOne(ctx) {
+    const { id } = ctx.params
+
+    const entity = await strapi.db.query('api::project.project').findOne({
+      where: { redirect: id }
+    })
+
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx)
+
+    return this.transformResponse(sanitizedEntity);
+  }
+}))
