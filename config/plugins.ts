95aaba989f0d40b0292e6884a61ba79a4875a3e0
NakaokaGabriel <gabrielnakaoka_99@hotmail.com>
2024-04-03 23:13:10 -0300

    feat: :building_construction: added amazon s3 storage to files

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,4 +1,4 @@
-export default () => ({
+export default ({ env }) => ({
   graphql: {
     enabled: true,
     config: {
@@ -11,5 +11,28 @@ export default () => ({
         tracing: false,
       },
     }
-  }
+  },
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        credentials: {
+          accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+          secretAccessKey: process.env.AWS_ACCESS_SECRET,
+        },
+        region: process.env.AWS_REGION,
+        baseUrl: `https://s3.${process.env.AWS_REGION}.amazonaws.com/${process.env.AWS_BUCKET}`, // This line sets the custom url format
+        params: {
+          ACL: process.env.AWS_ACL || 'public-read',
+          signedUrlExpires: process.env.AWS_SIGNED_URL_EXPIRES || 15 * 60,
+          Bucket: process.env.AWS_BUCKET,
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
 })
